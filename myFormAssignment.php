93beffa4d2cd56129b5e7d1e5c3aae14cf8fe9ef
JamesBogert26 <jbb26@njit.edu>
2019-10-08 10:51:37 -0400

    adding myFormAssignment.php

--- myFormAssignment.php
+++ myFormAssignment.php
@@ -1,35 +1,43 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function getName(){
 	if(isset($_GET['name'])){
 		echo "<p>Hello, " . $_GET['name'] . "</p>";
 	}
 }
+
+$password = $_GET['password'];
+$confirmpassword = $_GET['confirm_password'];
+
+if($_GET['password']==$_GET['confirm_password'])
+{
+	echo "<p> Confirmed" . "</p>";
+}
+else
+{
+	echo "<p> Doesnt match" . "</p>";
+}
 ?>
 <html>
 <head></head>
 <body><?php getName();?>
-<form mode="GET" action="#">
+<form method="GET" action="#">
 <input name="name" type="text" placeholder="Enter your name"/>
-<!--new content-->
-<input name="number" type="number" placeholder="Enter a number"/>
-<input name="password" type="password" placeholder="Enter a FAKE password"/>
+<input name="password" type="password" placeholder="Enter a password"/>
 
-<label for="Yes">Yes</label><!-- for="id of element"-->
-<input type="radio" name="radio" id="Yes" value="Yes"/>
-<label for="No">No</label>
-<input type="radio" name="radio" id="No" value="No"/>
-<!-- this is a comment -->
-<select name="dropdown">
-	<option value="1">One</option>
-	<option value="2">Two</option>
-	<option value="3">Three</option>
-</select>
-<input type="checkbox" name="check"/>
-<textarea name="text"></textarea>
-<input type="date" name="date"/>
-<!--end new content-->
+<input name="password" type="password" placeholder="Confirm your password"/>
+
+<button type="submit" class="pure-button pure-button-primary">Confirm</button>
+
+
+
+
+
+		
+
 <input type="submit" value="Try it"/>
-<input type="reset" value="Clear Form"/>
 </form>
 </body>
 </html>
@@ -38,3 +46,4 @@ function getName(){
 if(isset($_GET)){
 	echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
 }
+?>
